1f7757f630ac76863eadaeb97a97db22b3b13a34
Angel Guerrero <ajguerreror@gmail.com>
2021-04-29 13:28:05 -0400

    Gestiona componentes con angular cli

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule
